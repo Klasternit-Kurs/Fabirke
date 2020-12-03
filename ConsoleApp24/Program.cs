7912e1d486a4c013b9ba2fa6748b323a570c9733
Dragan Pepic <d.pepic@hotmail.com>
2020-12-03 18:42:31 +0100

    Add project files.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RealSpawner rs = new RealSpawner(Neprijatelji.Creep);
+			rs.DajNesto();
+		}
+	}
+	public class RealSpawner
+	{
+		private readonly EnemySpawner es;
+
+		public RealSpawner(Neprijatelji n)
+		{
+			switch(n)
+			{
+				case Neprijatelji.Creep:
+					es = new NestoDrugo();
+					break;
+			}
+		}
+
+		public Neprijatelj DajNesto()
+			=> es.Spawn();
+	}
+
+
+	//Cocrete Producer
+	public class NestoDrugo : EnemySpawner
+	{
+		public override Neprijatelj Spawn()
+		{
+			return new Novi();
+		}
+	}
+
+	public enum Neprijatelji
+	{
+		Creep,
+		NestoDrug
+	}
+
+	//Producer
+	public abstract class EnemySpawner
+	{
+		public abstract Neprijatelj Spawn();
+	}
+
+
+	//Concrete Product
+	public class Novi : Neprijatelj
+	{
+		public override void OstetiIgraca()
+		{
+			throw new NotImplementedException();
+		}
+	}
+	public class Melee : Neprijatelj
+	{
+		public string mac;
+		public int jacina;
+		public override void OstetiIgraca()
+		{
+			throw new NotImplementedException();
+		}
+	}
+	public class Puca : Neprijatelj
+	{
+		public string Oruzje;
+		public int Domet;
+		public override void OstetiIgraca()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	//Product
+	public abstract class Neprijatelj
+	{
+		public int hp;
+		public abstract void OstetiIgraca();
+	}
+}
